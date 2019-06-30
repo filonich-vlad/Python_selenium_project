1f65d92a769fbdb5f5f6e4be76c762e30fb48fd7
Vladislav <filonich.v@mail.ru>
2019-07-01 01:32:48 +0300

    Main page test refactored to more object oriented style.

--- test_main_page.py
+++ test_main_page.py
@@ -1,9 +1,12 @@
 from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
 
 def test_guest_can_go_to_login_page(browser):
-    #link = "http://selenium1py.pythonanywhere.com/"
-    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer "
-    page = MainPage(browser, link) 
-    page.open()
-    page.go_to_login_page()
-    page.should_be_login_link()
+    link = "http://selenium1py.pythonanywhere.com/"
+    #link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer "
+    main_page = MainPage(browser, link) 
+    main_page.open()
+    main_page.should_be_login_link()
+    main_page.go_to_login_page()
+    login_page =  LoginPage(broswer, browser.current_url)
+    login_page.should_be_login_page()
