03493b325004a809eb9693f2d7137add435d88f4
Vladislav <filonich.v@mail.ru>
2019-07-02 09:42:56 +0300

    3 more tests on product page added.

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,14 +1,20 @@
-from selenium.common.exceptions import NoSuchElementException, NoAlertPresentException
-import math
+from selenium.common.exceptions import NoSuchElementException, \
+                                NoAlertPresentException, TimeoutException
+from math import log, sin
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 
 class BasePage:
     def __init__(self, browser, url, timeout=10):
         self.browser = browser
         self.url = url
         self.browser.implicitly_wait(timeout)
+
         
     def open(self):
         self.browser.get(self.url)
+
 
     def is_element_present(self, how, what):
         try:
@@ -16,11 +22,12 @@ class BasePage:
         except NoSuchElementException:
             return False
         return True
+
                 
     def solve_quiz_and_get_code(self):
         alert = self.browser.switch_to.alert
         x = alert.text.split(" ")[2]
-        answer = str(math.log(abs((12 * math.sin(float(x))))))
+        answer = str(log(abs((12 * sin(float(x))))))
         alert.send_keys(answer)
         alert.accept()
         try:
@@ -29,3 +36,20 @@ class BasePage:
             alert.accept()
         except NoAlertPresentException:
             print("No second alert presented")
+
+
+    def is_not_element_present(self, how, what, timeout=4):
+        try:
+            WebDriverWait(self.browser, timeout).until(EC.presence_of_element_located((how, what)))
+        except TimeoutException:
+            return True
+        return False
+
+
+    def is_disappeared(self, how, what, timeout=4):
+        try:
+            WebDriverWait(self.browser, timeout, 1, TimeoutException).\
+                until_not(EC.presence_of_element_located((how, what)))
+        except TimeoutException:
+            return False
+        return True
--- test_product_page.py
+++ test_product_page.py
@@ -1,9 +1,10 @@
 import pytest
 from .pages.product_page import ProductPage
+from .pages.locators import ProductPageLocators as PPL
 
 
-product_base_link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
-links = [f"{product_base_link}/?promo=offer{i}" for i in range(10)]
+product_link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
+links = [f"{product_link}/?promo=offer{i}" for i in range(10)]
 
 @pytest.mark.parametrize('link', links)
 def test_guest_can_add_product_to_cart(browser, link):
@@ -15,3 +16,33 @@ def test_guest_can_add_product_to_cart(browser, link):
     product_page.product_name_in_message_is_the_same_as_one_in_the_cart()
     product_page.messege_of_price_of_the_cart_is_present()
     product_page.price_of_the_cart_coincides_with_the_price_of_the_product()
+
+
+@pytest.mark.xfail(reason="Alert rises instantly.")
+@pytest.mark.late
+def test_guest_cant_see_success_message_after_adding_product_to_cart(browser):
+    global product_link
+    product_page = ProductPage(browser, product_link)
+    product_page.open()
+    product_page.add_to_cart()
+    product_page.solve_quiz_and_get_code(browser)
+    product_page.is_not_element_present(*PPL.PRODUCT_ADDED_TO_CART_ALERT)
+
+    
+@pytest.mark.late
+def test_guest_cant_see_success_message(browser):
+    global product_link
+    product_page = ProductPage(browser, product_link)
+    product_page.open()
+    product_page.is_not_element_present(*PPL.PRODUCT_ADDED_TO_CART_ALERT)
+    
+
+@pytest.mark.xfail(reason="The alert should stay.")
+@pytest.mark.late
+def test_message_dissapeared_after_adding_product_to_cart(browser):
+    global product_link
+    product_page = ProductPage(browser, product_link)
+    product_page.open()
+    product_page.add_to_cart()
+    product_page.solve_quiz_and_get_code()
+    product_page.is_disappeared(*PPL.PRODUCT_ADDED_TO_CART_ALERT)
